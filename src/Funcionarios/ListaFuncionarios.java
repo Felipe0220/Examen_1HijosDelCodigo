b828ad3c80031c5d6e07be0bf60d7e18e8ec154f
DavidSalazar <jdrsalazar1025@gmail.com>
2025-07-03 16:00:19 -0600

    clase ListaFuncionario
    se crea la clase lista de funcionario, se lograron implementar los métodos abstractos , de la clase abstracta LIST, pero no se programaron los métodos en la clase ListaFuncionario

---
+++ src/Funcionarios/ListaFuncionarios.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Funcionarios;
+
+import Listas.Lista;
+
+/**
+ *
+ * @author UTN
+ */
+public class ListaFuncionarios implements Lista<Funcionario>{
+private static Funcionario list[];
+private int i;
+    @Override
+    public Funcionario Buscar(Object id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean eliminar(Funcionario s) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean agregar(Funcionario s) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
