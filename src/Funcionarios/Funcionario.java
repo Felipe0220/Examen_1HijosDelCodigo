d841a3e8aa981638aa918c5aeeaa8cc8b4fea8c2
DavidSalazar <jdrsalazar1025@gmail.com>
2025-07-03 14:42:23 -0600

    clase funcionario
    se crea la clase funcionario  que hereda de persona el nombre y la cedula

---
+++ src/Funcionarios/Funcionario.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Funcionarios;
+
+/**
+ *
+ * @author UTN
+ */
+public class Funcionario extends Personas {
+    private String departamento;
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+    
+    
+}
