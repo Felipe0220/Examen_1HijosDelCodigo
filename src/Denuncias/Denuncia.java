639a150c2e4f7b5396bd902ad049c6079de0e8c7
Kevin Quiros <kevinquiros.m@gmail.com>
2025-07-03 14:39:18 -0600

    Se crea la clase Denuncia
    Se crea la clase con los metodos de acceso pero sin el constructor

---
+++ src/Denuncias/Denuncia.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Denuncias;
+
+/**
+ *
+ * @author Student
+ */
+public class Denuncia {
+   private String codigo;
+   private String descripcion;
+   private String provincia ;
+   private String canton;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public String getCanton() {
+        return canton;
+    }
+
+    public void setCanton(String canton) {
+        this.canton = canton;
+    }
+   
+   
+   
+   
+   
+}
